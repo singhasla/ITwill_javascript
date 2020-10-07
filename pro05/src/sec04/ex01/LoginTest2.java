8b4e20e29c8251605d91c6fc9eda4f6e0fadd279
singhasla <s1n9hasla@gmail.com>
2020-10-07 11:58:01 +0900

    로그인 페이지 서블릿 이해하기

---
+++ pro05/src/sec04/ex01/LoginTest2.java
@@ -0,0 +1,72 @@
+package sec04.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/loginTest2")	//http://localhost:8080/pro05/
+public class LoginTest2 extends HttpServlet {
+
+	// 1순위 호출
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("톰캣 메모리에 LoginTest서블릿 클래스가 로드 되는 시점에 호출됨");
+	}
+
+	// 2순위 호출
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// 1. 클라이언트가 입력한 아이디 비밀번호를 얻기 전에 한글인코딩방식 지정
+		request.setCharacterEncoding("utf-8");
+		
+		// 2. 클라이언트가 입력한 아이디, 비밀번호를 request 내장객체 메모리 영역에서 꺼내오기
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		
+		// 3. 클라이언트의 웹브라우저로 응답할 값 생성
+			//순서1. 클라이언트의 웹브라우저로 응답할 데이터 종류(MIME-TYPE) 설정
+		response.setContentType("text/html;charset=utf-8");
+			//순서2. 클라이언트의 웹브라우저로 응답(출력)할 출력스트림 통로 얻기 
+		PrintWriter out = response.getWriter();
+			//순서3. 응답할 값 생성하고 클라이언트의 웹브라우저로 응답(출력)
+		if( id != null && (id.length() != 0) ){	//아이디를 입력했다면
+			
+			if(id.equals("admin")){	//관리자계정(admin)으로 입력했다면 관리자 화면으로 응답
+				out.print("<html>");
+				out.print("<body>");
+					out.print("<font size='12'>"+ id + "관리자님, 로그인 하셨습니다.<br>");
+					out.print("<input type='button' value='회원정보수정' />");
+					out.print("<input type='button' value='회원정보삭제' />");
+				out.print("</body>");
+				out.print("</html>");
+			} else {	//일반 계정으로 입력했다면 일반 사용자 화면으로 응답
+				out.print("<html>");
+				out.print("<body>");
+					out.print(id + "님, 로그인 하셨습니다.");
+				out.print("</body>");
+				out.print("</html>");
+			}
+			
+		} else {	//아이디를 입력 하지 않았다면
+			out.print("<html>");
+			out.print("<body>");
+				out.print("아이디를 입력하세요.<br>");
+				out.print("<a href='http://localhost:8080/pro05/test01/login2.html'> 로그인 페이지로 이동 </a>");
+			out.print("</body>");
+			out.print("</html>");
+		}
+
+	}
+
+	// 마지막 호출
+	public void destroy() {
+		System.out.println("톰캣을 중지하면(서블릿프로그램이 종료되면) 호출되는 메소드");
+	}
+
+	
+}
